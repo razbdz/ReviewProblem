3d124714458e5db27361110545c5aa7e52c2fb24
Razije <s236154@dtu.dk>
2025-11-10 15:06:10 +0100

    aflevering 10

---
+++ ReviewProblems  /Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System;
+
+namespace ArithmeticOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first integer");
+            int first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second integer");
+            int second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(first + second);
+            Console.WriteLine(first * second);
+            Console.WriteLine(first - second);
+            Console.WriteLine(first / second);
+            Console.WriteLine(first % second);
+        }
+    }
+}
+
+
+
+
+
